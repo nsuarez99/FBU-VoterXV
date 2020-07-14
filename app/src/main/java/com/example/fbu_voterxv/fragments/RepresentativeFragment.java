5ff5ead5fede8e429fcd0b748631f96531113fc6
nsuarez99 <ngsuarez99@gmail.com>
2020-07-14 18:55:54 -0400

    create representative fragment

---
+++ app/src/main/java/com/example/fbu_voterxv/fragments/RepresentativeFragment.java
@@ -0,0 +1,45 @@
+package com.example.fbu_voterxv.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.fbu_voterxv.R;
+
+public class RepresentativeFragment extends Fragment {
+
+    private ImageView image;
+    private TextView name;
+    private TextView party;
+    private TextView committee;
+    private TextView years;
+    private TextView fb;
+    private TextView twittter;
+    private TextView office;
+
+
+    public RepresentativeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_representative, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
