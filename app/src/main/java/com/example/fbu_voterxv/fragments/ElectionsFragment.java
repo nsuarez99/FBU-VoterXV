599f4dde82318138f713b0db619de066061231ed
nsuarez99 <ngsuarez99@gmail.com>
2020-07-14 16:32:56 -0400

    connected election fragment to candidate fragment

--- app/src/main/java/com/example/fbu_voterxv/fragments/ElectionsFragment.java
+++ app/src/main/java/com/example/fbu_voterxv/fragments/ElectionsFragment.java
@@ -5,17 +5,30 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.os.Parcel;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.fbu_voterxv.R;
+import com.example.fbu_voterxv.adapters.ElectionAdapter;
+import com.example.fbu_voterxv.models.Election;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ElectionsFragment extends Fragment {
 
     private RecyclerView recyclerView;
+    private List<Election> elections;
+    private ElectionAdapter adapter;
 
     public ElectionsFragment() {}
 
@@ -29,5 +42,40 @@ public class ElectionsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+
+        final FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        final Fragment candidatesFragment = new CandidatesFragment();
+
+        //setup recyclerview onclick listener
+        ElectionAdapter.OnClickListener onClickListener = new ElectionAdapter.OnClickListener() {
+            @Override
+            public void onClick(int position) {
+                Election election = elections.get(position);
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("election", Parcels.wrap(election));
+                candidatesFragment.setArguments(bundle);
+                fragmentManager.beginTransaction().replace(R.id.frameLayoutContainer, candidatesFragment).commit();
+            }
+        };
+
+        //set adapter and recycler view
+        elections = new ArrayList<>();
+        adapter = new ElectionAdapter(getContext(), elections, onClickListener);
+        recyclerView.setAdapter(adapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        //TODO add endless scrolling
+//        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+//            @Override
+//            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+//                loadNextQueryPosts(elections.size());
+//            }
+//        };
+//        recyclerView.addOnScrollListener(scrollListener);
+
     }
 }
